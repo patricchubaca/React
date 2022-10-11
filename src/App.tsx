93a9b3067c7e9ad194cbb8f18d261ac8fb9867a9
Patric Enderson <patric@patric.enderson>
2022-10-11 11:03:48 -0300

    Feat :: Drop archives

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,10 @@
-import './App.css';
 import Banner from './Componentes/Banner/Banner';
 
 function App() {
   return (
     <div className="App">
       <Banner />
-      <header className="App-header">
-        <h1>Patric Reativo</h1>
 
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
