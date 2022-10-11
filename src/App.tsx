d4e5988c2e16e320c23135c1f8044b6e723f4cb4
Patric Enderson <patric@patric.enderson>
2022-10-10 21:26:50 -0300

    Feat :: Init Component Banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Banner from './Componentes/Banner/Banner';
 
 function App() {
   return (
     <div className="App">
+      <Banner />
       <header className="App-header">
+        <h1>Patric Reativo</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
