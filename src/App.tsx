99441d45426957596dc150306caa09acd49f0b1c
Patric Enderson <patric@patric.enderson>
2022-10-11 11:29:49 -0300

    Feat :: Drop archives

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import Banner from './Componentes/Banner/Banner';
+import CampoTexto from './Componentes/CampoTexto/CampoTexto';
 
 function App() {
   return (
     <div className="App">
       <Banner />
-
+      <CampoTexto />
     </div>
   );
 }
---
+++ src/Componentes/CampoTexto/CampoTexto.tsx
@@ -0,0 +1,10 @@
+const CampoTexto = () => {
+  return (
+    <div>
+      <label>Nome</label>
+      <input type="text" name="nome" placeholder="Nome" />
+    </div>
+  )
+}
+
+export default CampoTexto
