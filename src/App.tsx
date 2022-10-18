9d023a6b019a9f12a4e6c39ac2255c4010ae02ca
Patric Enderson <patric@patric.enderson>
2022-10-18 10:16:52 -0300

    Feat :: Remove import CampoTexto da App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import Banner from './Componentes/Banner/IndexBanner';
-import CampoTexto from './Componentes/CampoTexto/IndexCampoTexto';
 import Formulario from './Componentes/Formulario/IndexFormulario';
 
 function App() {
