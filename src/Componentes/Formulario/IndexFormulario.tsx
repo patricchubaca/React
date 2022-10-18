65f51045304ec538922408fbe66b9f318e928401
Patric Enderson <patric@patric.enderson>
2022-10-18 10:31:13 -0300

    Feat :: Development Form

--- src/Componentes/Formulario/IndexFormulario.tsx
+++ src/Componentes/Formulario/IndexFormulario.tsx
@@ -4,13 +4,13 @@ import "./Formulario.css"
 
 function Formulario() {
   return (
-    <section >
-      <form >
+    <section className="formulario">
+      <form>
         <h2>Preencha os dados para criar o card do colaborador.</h2>
-        <CampoTexto label="Nome" placeholder="Digite seu nome" />
-        <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
-        <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
-        <CampoTexto label="Time" placeholder="Digite os integrantes do seu time" />
+        <CampoTexto label="Nome" placeholder="Digite seu nome..." />
+        <CampoTexto label="Cargo" placeholder="Digite seu cargo..." />
+        <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem..." />
+        <CampoTexto label="Time" placeholder="Digite os integrantes do seu time..." />
       </form>
     </section>
 
